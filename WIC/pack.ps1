b8c34fd45df0197d3537cd040067a9df85182b6c
sbaeumlisberger <s.baeumlisberger@live.de>
2025-01-12 17:06:26 +0100

    add assembly signing and make build deterministic

---
+++ WIC/pack.ps1
@@ -0,0 +1,24 @@
+$gitStatus = git status --porcelain
+
+if ($gitStatus) {
+    Write-Host "There are uncommitted changes in the repository. Please commit or stash them before running this script."
+    exit 1
+}
+
+$currentBranch = git rev-parse --abbrev-ref HEAD
+
+if ($currentBranch -ne "master") {
+    Write-Host "You are not on the master branch. Please switch to the master branch before running this script."
+    exit 1
+}
+
+git fetch
+
+$gitStatus = git status
+
+if ($gitStatus -match "Your branch is ahead") {
+    Write-Host "There are local commits that have not been pushed to the remote."
+    exit 1
+}
+
+dotnet pack /p:ContinuousIntegrationBuild=true /p:SignAssembly=true /p:AssemblyOriginatorKeyFile="D:\Dateien\Development\Zertifikate\wic-strong-name.snk"
